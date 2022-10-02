7dc6b4bfc978a3515de053c1fac10220ce594366
KateTref <trefilovaekaterina408@gmail.com>
2022-10-03 01:18:21 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
 
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+const DELAY = 1000;
+let intervalId = null;
+
+btnStart.addEventListener('click', onChangeBgColor);
+btnStop.addEventListener('click', offChangeBgColor);
+
+btnStop.setAttribute("disabled", "");
+
+function onChangeBgColor() {
+    if (btnStop.hasAttribute("disabled")) {
+        btnStop.removeAttribute("disabled");
+    }
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, DELAY);
+    btnStart.setAttribute("disabled", "");
+
+}
+
+function offChangeBgColor() {
+    clearInterval(intervalId);
+    btnStop.setAttribute("disabled", "");
+    btnStart.removeAttribute("disabled");
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
