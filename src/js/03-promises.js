0ee6cb5a09b3e3e67261e3cc6099c6d72c78b652
KateTref <trefilovaekaterina408@gmail.com>
2022-10-08 23:59:03 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+const formRef = document.querySelector(".form");
+
+formRef.addEventListener("submit", (event) => {
+  
+  console.log("hello");
+  preventDefault();
+})
+
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
