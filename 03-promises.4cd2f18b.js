3e1bfc5f8df4f69356f282313c61539400d12c6c
KateTref <96999008+KateTref@users.noreply.github.com>
2022-10-08 20:59:32 +0000

    Deploying to gh-pages from @ KateTref/goit-js-hw-09@0ee6cb5a09b3e3e67261e3cc6099c6d72c78b652 🚀

---
+++ 03-promises.4cd2f18b.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(e=>{console.log("hello"),preventDefault()}));
+//# sourceMappingURL=03-promises.4cd2f18b.js.map
---
+++ 03-promises.ffc757de.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){console.log("hello"),preventDefault()}));
+//# sourceMappingURL=03-promises.ffc757de.js.map
