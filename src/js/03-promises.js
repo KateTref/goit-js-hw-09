8b41f66a8edb24a2a6bcc615b6108ac2cb4a7997
KateTref <trefilovaekaterina408@gmail.com>
2022-10-09 10:26:24 +0300

    +

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,31 @@
 const formRef = document.querySelector(".form");
+const inputDelayRef = document.querySelector("[name=delay]");
+const inputStepRef = document.querySelector("[name=step]");
+const inputAmountRef = document.querySelector("[name=amount]");
 
-formRef.addEventListener("submit", (event) => {
+
+formRef.addEventListener("submit", createPromise);
   
-  console.log("hello");
-  preventDefault();
-})
+ 
+
+function createPromise(position, delay) {
+  function onClick(event){
+    event.preventDefault();
+  const delay = Number(inputDelayRef.value);
+  const step = Number(inputStepRef.value);
+  const amount = Number(inputAmountRef.value);
+  } 
+}
 
 
 
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+function createPromise(position, delay) {
+  
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+  } else {
+    // Reject
+  }
+}
 
