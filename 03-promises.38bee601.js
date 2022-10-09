a9c5b7a42a6b1b46644f00d9b1783ab3e9d2c84d
KateTref <96999008+KateTref@users.noreply.github.com>
2022-10-09 08:40:16 +0000

    Deploying to gh-pages from @ KateTref/goit-js-hw-09@a0714d4989074cceb8008e477b76bf8656090c1d 🚀

--- 03-promises.38bee601.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]");e.addEventListener("submit",(function(e,t){Math.random()}));
-//# sourceMappingURL=03-promises.38bee601.js.map
--- 03-promises.f18a83b2.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form");document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]");e.addEventListener("submit",(function(e,t){Math.random()}))}();
-//# sourceMappingURL=03-promises.f18a83b2.js.map
