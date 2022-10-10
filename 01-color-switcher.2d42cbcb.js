b40b8cc1cc7edb59cdb9661ca08d19f86d50c449
KateTref <96999008+KateTref@users.noreply.github.com>
2022-10-10 17:07:47 +0000

    Deploying to gh-pages from @ KateTref/goit-js-hw-09@6e64f0f36c399bb55ae1cee7b6ec64b71ec5e9b7 🚀

---
+++ 01-color-switcher.2d42cbcb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){e.removeAttribute("disabled"),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(n),e.setAttribute("disabled",""),t.removeAttribute("disabled")})),e.setAttribute("disabled","")}();
+//# sourceMappingURL=01-color-switcher.2d42cbcb.js.map
